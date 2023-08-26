0ebf76759dc6f23b38c72b56cad65dadceab842d
Mars <marseclarke@gmail.com>
2023-08-25 22:54:27 -0400

    dedenne uses akabeko

--- src/main/java/pokemonmaster/cards/Lightning/Dedenne.java
+++ src/main/java/pokemonmaster/cards/Lightning/Dedenne.java
@@ -2,9 +2,13 @@ package pokemonmaster.cards.Lightning;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import pokemonmaster.CustomTags;
 import pokemonmaster.cards.BaseCard;
 import pokemonmaster.jar.PokemonMaster;
@@ -44,12 +48,20 @@ public class Dedenne extends BaseCard {
     public void triggerOnManualDiscard() {
         super.onMoveToDiscard();
         addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.LIGHTNING));
+        AbstractPower POW = AbstractDungeon.player.getPower(VigorPower.POWER_ID);
+        if (POW != null){
+            addToBot(new RemoveSpecificPowerAction(AbstractDungeon.player, AbstractDungeon.player, POW));
+        }
     }
 
     @Override
     public void triggerWhenDrawn() {
         super.triggerWhenDrawn();
         addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.LIGHTNING));
+        AbstractPower POW = AbstractDungeon.player.getPower(VigorPower.POWER_ID);
+        if (POW != null){
+            addToBot(new RemoveSpecificPowerAction(AbstractDungeon.player, AbstractDungeon.player, POW));
+        }
     }
 
     @Override
